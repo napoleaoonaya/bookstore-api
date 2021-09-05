a8b0183727ec540b332d7a43f777e5568646ddb9
napoleaoonaya <napoleao.onaya@gmail.com>
2021-09-05 15:21:24 -0300

    classes de dominio

---
+++ src/main/java/br/com/onaya/bookstore/domain/Livro.java
@@ -0,0 +1,87 @@
+package br.com.onaya.bookstore.domain;
+
+public class Livro {
+	
+	private int id;
+	private String titulo;
+	private String nomeAutor;
+	private String texto;
+	
+	Categoria categoria;
+	
+	public Livro() {
+		super();
+	}
+
+	public Livro(int id, String titulo, String nomeAutor, String texto, Categoria categoria) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.nomeAutor = nomeAutor;
+		this.texto = texto;
+		this.categoria = categoria;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livro other = (Livro) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+}
