601a3e6db07e78f8f26d30ffbed906e974ecb2e6
napoleaoonaya <napoleao.onaya@gmail.com>
2021-09-05 23:02:00 -0300

    (FindById para Categoria)

---
+++ src/main/java/br/com/onaya/bookstore/service/CategoriaService.java
@@ -0,0 +1,21 @@
+package br.com.onaya.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.onaya.bookstore.domain.Categoria;
+import br.com.onaya.bookstore.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+		return obj.orElse(null);
+	}
+}
