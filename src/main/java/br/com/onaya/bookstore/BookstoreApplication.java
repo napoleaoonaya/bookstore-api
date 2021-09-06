aa4679b1c355db539c1f3e61a25dc6a68b1c4c63
napoleaoonaya <napoleao.onaya@gmail.com>
2021-09-05 22:39:45 -0300

    "Criando ambiente de dev"

--- src/main/java/br/com/onaya/bookstore/BookstoreApplication.java
+++ src/main/java/br/com/onaya/bookstore/BookstoreApplication.java
@@ -2,8 +2,12 @@ package br.com.onaya.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"br.com.onaya.bookstore"})
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
