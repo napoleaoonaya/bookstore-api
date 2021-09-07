ac5c427b986157d7941f39f5cfc87ab254c64a71
napoleaoonaya <napoleao.onaya@gmail.com>
2021-09-06 23:53:27 -0300

    (Implementação do endpoint de create)

--- src/main/java/br/com/onaya/bookstore/resource/CategoriaResource.java
+++ src/main/java/br/com/onaya/bookstore/resource/CategoriaResource.java
@@ -1,5 +1,6 @@
 package br.com.onaya.bookstore.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.onaya.bookstore.domain.Categoria;
 import br.com.onaya.bookstore.dto.CategoriaDTO;
@@ -33,5 +37,15 @@ public class CategoriaResource {
 		List<CategoriaDTO> categoriaDTO = categorias.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(categoriaDTO);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = categoriaService.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		//Salva a categoria e devolve a uri
+		//return ResponseEntity.created(uri).body(obj);
+		
+		return ResponseEntity.created(uri).build();
+	}
 }
